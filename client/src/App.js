b02dea0609a64aed1969a387f8dea407a2460481
Caroline Ninganga <carolninganga@gmail.com>
2020-07-03 14:27:58 -0400

    added required min-length

--- client/src/App.js
+++ client/src/App.js
@@ -5,29 +5,33 @@ import About from './components/pages/About';
 import Navbar from './components/layouts/Navbar';
 import Register from './components/auth/Register';
 import Login from './components/auth/Login';
-
+import Alerts from './components/layouts/Alerts';
 
 import ContactState from './context/contact/ContactState';
 import AuthState from './context/auth/AuthState';
+import AlertState from './context/alert/AlertState';
 import './App.css';
 
 const App = () => {
   return (
 <AuthState>
   <ContactState>
+    <AlertState>
     <Router>
       <Fragment>
         <Navbar />
           <div className="container">
+          <Alerts />
             <Switch>
               <Route exact path='/' component={Home} />
               <Route exact path='/about' component={About} />
               <Route exact path='/register' component={Register} />
               <Route exact path='/login' component={Login} />
-            </Switch>
+              </Switch>
           </div>
       </Fragment>
     </Router>
+    </AlertState>
   </ContactState>
 </AuthState>
   );
--- client/src/components/auth/Register.js
+++ client/src/components/auth/Register.js
@@ -1,6 +1,11 @@
-import React, { useState }from 'react';
+import React, { useState, useContext }from 'react';
+import AlertContext from '../../context/alert/alertContext';
 
 const Register = () => {
+    const alertContext = useContext(AlertContext);
+
+    const { setAlert } = alertContext;
+
     const [user, setUser] = useState({
         name: '',
         email: '',
@@ -14,8 +19,14 @@ const Register = () => {
 
     const onSubmit = e => {
         e.preventDefault();
+        if(name === '' || email === '' || password === '') {
+            setAlert('Please enter all fields', 'danger')
+        } else if (password !== password2) {
+            setAlert('Passwords do not match', 'danger');
+        } else {
         console.log('Register submit');
     }
+}
     return (
         <div className='form-container'>
         <h1>
@@ -28,15 +39,15 @@ const Register = () => {
             </div>
             <div className="form-group">
                 <label htmlFor="email">Email Address</label>
-                <input type="email" name="email" value={email} onChange={onChange}/>
+                <input type="email" name="email" value={email} onChange={onChange} required/>
             </div>
             <div className="form-group">
                 <label htmlFor="password">Password</label>
-                <input type="password" name="password" value={password} onChange={onChange}/>
+                <input type="password" name="password" value={password} onChange={onChange} required minLength="6"/>
             </div>
             <div className="form-group">
                 <label htmlFor="password2">Confirm Password</label>
-                <input type="password" name="password2" value={password2} onChange={onChange}/>
+                <input type="password" name="password2" value={password2} onChange={onChange} requires minLength="6"/>
             </div>
             <input type="submit" value="Register" className="btn btn-primary btn-block" />
         </form>
