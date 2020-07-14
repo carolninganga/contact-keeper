bd1cdf288b5e4cc1725f01b51f7d78c9ab12479e
Caroline Ninganga <carolineninganga@Carolines-MBP.home>
2020-07-14 15:03:35 -0400

    tutoring deploy

--- config/db.js
+++ config/db.js
@@ -1,22 +1,19 @@
-const mongoose = require('mongoose');
-const config = require('config');
-const db = config.get('mongoURI');
+const mongoose = require("mongoose");
+const db = require("./default.json");
 
 const connectDB = async () => {
-    try {
-        await mongoose
-        .connect(db, {
-            useUnifiedTopology: true,
-            useNewUrlParser: true,
-            useCreateIndex: true,
-            useFindAndModify:false
-        });
-        console.log('MongoDB Connected...')
-    } catch (err) {
-        console.error(err.message);
-        process.exit(1)
-
-    }    
-}
+  try {
+    await mongoose.connect(db.mongoURI, {
+      useUnifiedTopology: true,
+      useNewUrlParser: true,
+      useCreateIndex: true,
+      useFindAndModify: false,
+    });
+    console.log("MongoDB Connected...");
+  } catch (err) {
+    console.error(err.message);
+    process.exit(1);
+  }
+};
 
 module.exports = connectDB;
