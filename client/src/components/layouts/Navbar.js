98231850bac0313868a17bcee457fb1c66ffe413
Caroline Ninganga <carolineninganga@Carolines-MBP.home>
2020-07-15 20:33:13 -0400

    added to heroku

--- client/src/components/layouts/Navbar.js
+++ client/src/components/layouts/Navbar.js
@@ -35,6 +35,9 @@ const Navbar = ({ title, icon }) => {
             <li>
                 <Link to='/login'>Login</Link>
             </li>
+            <li>
+                <Link to='/about'>About</Link>
+            </li>
         </Fragment>
     );
 
